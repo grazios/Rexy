f0e8228f96fd01db542411a5171150c5e722b213
Sugiyama Kouichi <a.k.a.cryptomeria@gmail.com>
2015-12-27 12:34:00 +0900

    edit create history

--- public/assets/javascripts/rexy.js
+++ public/assets/javascripts/rexy.js
@@ -1,69 +1,73 @@
 (function() {
-  "use strict";
-  $(document).ready(function() {
-    var eventHTML, events, opt;
-    events = [
-      {
-        id: 1,
-        year: 1988,
-        month: 7,
-        day: 20,
-        subject: "武山生誕"
-      }, {
-        id: 3,
-        year: 1990,
-        month: 12,
-        day: 26,
-        subject: "ほげほげ"
-      }, {
-        id: 3,
-        year: 2000,
-        month: 12,
-        day: 26,
-        subject: "ほげほげ"
-      }, {
-        id: 5,
-        year: 2005,
-        month: 4,
-        day: 1,
-        subject: "大学"
-      }, {
-        id: 5,
-        year: 2006,
-        month: 4,
-        day: 1,
-        subject: "大学"
-      }, {
-        id: 4,
-        year: 2007,
-        month: 4,
-        day: 1,
-        subject: "大学"
-      }, {
-        id: 2,
-        year: 2015,
-        month: 12,
-        day: 26,
-        subject: "開発合宿"
-      }
-    ];
-    opt = {
-      startYear: events[0].year,
-      endYear: events[events.length - 1].year,
-      range: events[events.length - 1].year - events[0].year,
-      showCaseWidth: $("#hoge").width()
-    };
-    eventHTML = "";
-    $.each(events, function(i, event) {
-      var left;
-      left = i === 0 ? 0 : 100;
-      if (i !== event.length - 1) {
-        left = opt.showCaseWidth / opt.range * (event.year - opt.startYear);
-      }
-      return eventHTML = eventHTML + ("<li style='left:" + left + "px' id='event_" + event.id + "'>" + event.year + " - " + event.subject + "</li>\r\n");
+  (function() {
+    'use strict';
+    $(document).ready(function() {
+      var eventHTML, events, opt;
+      eventHTML = void 0;
+      events = void 0;
+      opt = void 0;
+      events = [
+        {
+          id: 1,
+          year: 1988,
+          month: 7,
+          day: 20,
+          subject: '武山生誕'
+        }, {
+          id: 3,
+          year: 1990,
+          month: 12,
+          day: 26,
+          subject: 'ほげほげ'
+        }, {
+          id: 3,
+          year: 2000,
+          month: 12,
+          day: 26,
+          subject: 'ほげほげ'
+        }, {
+          id: 5,
+          year: 2005,
+          month: 4,
+          day: 1,
+          subject: '大学'
+        }, {
+          id: 5,
+          year: 2006,
+          month: 4,
+          day: 1,
+          subject: '大学'
+        }, {
+          id: 4,
+          year: 2007,
+          month: 4,
+          day: 1,
+          subject: '大学'
+        }, {
+          id: 2,
+          year: 2015,
+          month: 12,
+          day: 26,
+          subject: '開発合宿開発合宿開発合宿開発合宿開発合宿開発合宿開発合宿'
+        }
+      ];
+      opt = {
+        startYear: events[0].year,
+        endYear: events[events.length - 1].year,
+        range: events[events.length - 1].year - events[0].year,
+        showCaseWidth: $('.eventNav').width() - 48
+      };
+      eventHTML = '';
+      $.each(events, function(i, event) {
+        var left;
+        left = void 0;
+        if (i !== event.length - 1) {
+          left = opt.showCaseWidth / opt.range * (event.year - opt.startYear);
+        }
+        return eventHTML = eventHTML + '<li class=\'animated fadeIn\' style=\'left:' + left + 'px\' id=\'event_' + event.id + '\'>' + '<div class=\'time\'>' + event.year + '</div><div class=\'point\'></div>' + '<h3>' + event.subject + '</h3>' + '</li>\r\n';
+      });
+      return $('.eventNav ul').append(eventHTML);
     });
-    $("#hoge").append(eventHTML);
-    return console.log(opt);
-  });
+  }).call(this);
 
 }).call(this);
